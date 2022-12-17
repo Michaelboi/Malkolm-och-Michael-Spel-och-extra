bce439a67a0480da9b7d64830c84197f6b48aeeb
Michaelboi <micnwa050628@student.jenseneducation.se>
2022-12-17 13:57:08 +0100

    Ändrade movement lite

--- Spel med listor Malkolm, Michael/Assets/spelare_movement.cs
+++ Spel med listor Malkolm, Michael/Assets/spelare_movement.cs
@@ -3,61 +3,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 namespace spel_move
 {
     public class spelare_movement : MonoBehaviour
     {
         // Den här funktionen är till för att kunna använda kroppen av objectet och sätta den som en variabel
         public Rigidbody RB;
+        public float move_speed = 5;
+        public float max_speed = 20;
+        public Vector3 move_input;
         public void kub_movement()
         {
             RB = GetComponent<Rigidbody>();
             
+            
         }
 
-        // inputs för att spelaren ska kunna röra sig
+        // input för att spelaren ska kunna röra sig höger, vänster, fram, bak och uppåt
         public void movement()
         {
+            move_input = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            RB.drag = (move_speed / max_speed);
+            RB.AddForce(move_input * move_speed);
             
-            if (Input.GetKey(KeyCode.Space))
+
+            if (Input.GetButtonDown("Jump"))
             {
-                RB.AddForce(0, 5, 0);
-            }
-            if (Input.GetKeyUp("space"))
-            {
-                RB.AddForce(0, 0, 0);
-            }
-            if (Input.GetKey("up"))
-            {
-                RB.AddForce(0, 0, 5);
-            }
-            if (Input.GetKeyUp("up"))
-            {
-                RB.AddForce(0, 0, 0);
-            }
-            if (Input.GetKey("right"))
-            {
-                RB.AddForce(5, 0, 0);
-            }
-            if (Input.GetKeyUp("right"))
-            {
-                RB.AddForce(0, 0, 0);
-            }
-            if (Input.GetKey("down"))
-            {
-                RB.AddForce(0, 0, -5);
-            }
-            if (Input.GetKeyUp("down"))
-            {
-                RB.AddForce(0, 0, 0);
-            }
-            if (Input.GetKey("left"))
-            {
-                RB.AddForce(-5, 0, 0);
-            }
-            if (Input.GetKeyUp("left"))
-            {
-                RB.AddForce(0, 0, 0);
+                RB.velocity = new Vector3(RB.velocity.x, 5f, RB.velocity.z);
+                
             }
         }
     }
