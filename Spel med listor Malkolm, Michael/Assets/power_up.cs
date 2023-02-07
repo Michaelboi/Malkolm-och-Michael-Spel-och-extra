2077a0b4edb321ad179b75c495dd2e65c41b4c4a
nopplis <malnom050808@student.jenseneducation.se>
2023-02-07 13:59:50 +0100

    fungerande powerup efter 100 fucking timmar på riktogt jag hatar att koden aldrig funar som den ska

--- Spel med listor Malkolm, Michael/Assets/power_up.cs
+++ Spel med listor Malkolm, Michael/Assets/power_up.cs
@@ -1,32 +1,47 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using UnityEditor.Search;
 using UnityEngine;
 
 public class power_up : MonoBehaviour
 {
+    void Start()
+    {
+        //StartCoroutine(ShowAndHide());
+       
+    }
+
     public void OnTriggerEnter(Collider other)
     {
+
+        
         //kollar om den som kolliderar med oblektet har tagen bil
         if (other.CompareTag("Player"))
         {
             Pickup();
+            gameObject.SetActive(false);
+            Invoke("ReactivateObject", 3f);
         }
+    }
+    private void ReactivateObject()
+    {
+        gameObject.SetActive(true);
     }
     //är en funktion som kommer plocka upp powerupen
     public void Pickup()
     {
         Debug.Log("puss");
+        //ShowAndHide();
         //ska skaffa parkiklar som kommer när man åker igenom powerupen
         // Ge en effekt till spelaren
-        // Ta bort objektet under en viss period
-        gameObject.SetActive(false);
-        //Task.Delay(5);gameObject.SetActive(true);
-      
-        Thread.Sleep(3000);
-        gameObject.SetActive(true);
-
     }
-
+    ///public IEnumerator snopp()
+    //{
+    //gameObject.SetActive(false);
+    //yield return new WaitForSeconds(3);
+    //gameObject.SetActive(true)\\\
+    //}
 }
